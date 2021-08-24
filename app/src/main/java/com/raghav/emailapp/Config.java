6f2038bc3cbff32cd6d3d295591d5d3a92043973
RAGHAVENDRAN <raghavhari001@gmail.com>
2021-08-24 23:19:09 +0530

    Email App build with Java email sender API

--- app/src/main/java/com/raghav/emailapp/Config.java
+++ app/src/main/java/com/raghav/emailapp/Config.java
@@ -2,6 +2,6 @@ package com.raghav.emailapp;
 
 public class Config {
 
-    public static final String EMAIL ="raghavhari001@gmail.com";
-    public static final String PASSWORD ="raaa001bylee$";
+    public static final String EMAIL ="your_email";
+    public static final String PASSWORD ="your_password";
 }
